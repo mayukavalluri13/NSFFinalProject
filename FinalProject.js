21cf27bcb40add252c55e34600b4bbf505de4f1a
mayvalluri <35422888+mayvalluri@users.noreply.github.com>
2018-01-14 07:58:37 -0500

    Add files via upload

---
+++ FinalProject.js
@@ -0,0 +1,34 @@
+function lightPollution(){
+//alert("hello");
+window.location='Light Pollution.html';
+
+}
+
+function soundPollution(){
+//alert("hello");
+window.location='SoundPollution.html';
+
+}
+
+function back(){
+//alert("in back()");
+window.location='index.html';	
+}
+
+function waterPollution(){
+//alert("hello");
+window.location='WaterPollution.html';
+
+}
+
+function airPollution(){
+//alert("hello");
+window.location='AirPollution.html';
+
+}
+
+function landPollution(){
+//alert("hello");
+window.location='LandPollution.html';
+
+}
